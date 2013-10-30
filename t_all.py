2c3228c561916bda903092b56d09af431cfa6929
Bruce Cropley <cropleyb@yahoo.com.au>
2013-10-31 07:51:06 +1100

    Test suite for the whole lot...

---
+++ t_all.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+import unittest
+import t_pente
+import t_update_substrips
+import t_alpha_beta
+
+def suite():
+    suite1 = unittest.defaultTestLoader.loadTestsFromModule(t_pente)
+    suite2 = unittest.defaultTestLoader.loadTestsFromModule(t_update_substrips)
+    suite3 = unittest.defaultTestLoader.loadTestsFromModule(t_alpha_beta)
+    allTests = unittest.TestSuite()
+    allTests.addTest(suite1)
+    allTests.addTest(suite2)
+    allTests.addTest(suite3)
+    return allTests
+
+if __name__ == "__main__":
+    unittest.TextTestRunner().run(suite())
