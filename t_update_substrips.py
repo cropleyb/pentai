66c443422b74672079e404c74086ea0ebdbf3f81
Bruce Cropley <cropleyb@yahoo.com.au>
2013-10-30 12:05:46 +1100

    Added more tests, all working already

--- t_update_substrips.py
+++ t_update_substrips.py
@@ -69,6 +69,66 @@ class SubStripCountingTest(unittest.TestCase):
         self.assertEquals(self.us_counter.tup(), (0,1,2,0,0))
         self.assertEquals(self.them_counter.tup(), (0,0,0,0,0))
 
+    def test_count_double_split_three(self):
+        self.add_substrips_for_str("  U U U  ")
+        self.assertEquals(self.us_counter.tup(), (0,4,1,0,0))
+        self.assertEquals(self.them_counter.tup(), (0,0,0,0,0))
+
+    def test_count_split_four(self):
+        self.add_substrips_for_str("  UUU U  ")
+        self.assertEquals(self.us_counter.tup(), (0,1,3,1,0))
+        self.assertEquals(self.them_counter.tup(), (0,0,0,0,0))
+
+    def test_count_middle_split_four(self):
+        self.add_substrips_for_str("  UU UU  ")
+        self.assertEquals(self.us_counter.tup(), (0,2,2,1,0))
+        self.assertEquals(self.them_counter.tup(), (0,0,0,0,0))
+
+    def test_count_five_mid(self):
+        self.add_substrips_for_str("  UUUUU  ")
+        self.assertEquals(self.us_counter.tup(), (0,0,2,2,1))
+        self.assertEquals(self.them_counter.tup(), (0,0,0,0,0))
+
+    def test_count_opponent_five(self):
+        self.add_substrips_for_str("  TTTTT  ")
+        self.assertEquals(self.us_counter.tup(), (0,0,0,0,0))
+        self.assertEquals(self.them_counter.tup(), (0,0,2,2,1))
+
+    def test_count_five_side(self):
+        self.add_substrips_for_str("UUUUU    ")
+        self.assertEquals(self.us_counter.tup(), (1,1,1,1,1))
+        self.assertEquals(self.them_counter.tup(), (0,0,0,0,0))
+
+    def test_count_five_side_with_stopper(self):
+        self.add_substrips_for_str("UUUUUT   ")
+        self.assertEquals(self.us_counter.tup(), (0,0,0,0,1))
+        self.assertEquals(self.them_counter.tup(), (0,0,0,0,0))
+
+    def test_count_three_side_with_stopper(self):
+        self.add_substrips_for_str("UUU  T   ")
+        self.assertEquals(self.us_counter.tup(), (0,0,1,0,0))
+        self.assertEquals(self.them_counter.tup(), (2,0,0,0,0))
+
+    def test_pre_threaten_pair(self):
+        self.add_substrips_for_str("     TT  ")
+        self.assertEquals(self.us_counter.tup(), (0,0,0,0,0))
+        self.assertEquals(self.them_counter.tup(), (1,3,0,0,0))
+
+    def test_threaten_pair(self):
+        self.add_substrips_for_str("    UTT  ")
+        self.assertEquals(self.us_counter.tup(), (1,0,0,0,0))
+        self.assertEquals(self.them_counter.tup(), (0,0,0,0,0))
+
+    def test_threaten_two_pairs(self):
+        self.add_substrips_for_str("  TTUTT  ")
+        self.assertEquals(self.us_counter.tup(), (0,0,0,0,0))
+        self.assertEquals(self.them_counter.tup(), (0,0,0,0,0))
+
+    def test_block_four(self):
+        self.add_substrips_for_str("    UTTTT")
+        self.assertEquals(self.us_counter.tup(), (1,0,0,0,0))
+        self.assertEquals(self.them_counter.tup(), (0,0,0,0,0))
+
 if __name__ == "__main__":
     unittest.main()
 
