94074b7ac83383ebc149b643c0a35532afdc4ae4
Bruce Cropley <cropleyb@yahoo.com.au>
2013-10-31 18:38:53 +1100

    More shuffling

--- ab_bridge.py
+++ ab_bridge.py
@@ -19,116 +19,19 @@ class Pos():
                    self.tup[1] + (direction[1] * steps)) 
         return Pos(new_pos)
 
-
 class ABState():
+    """ Bridge for state, for use by alpha_beta code """
     def __init__(self, parent=None, move=None):
-        self.parent = parent
-        if parent == None:
-            self.turn = 0
-            self.captured = [0,0]
-            # self.board = [[0 for k in range(BOARD_SIZE)] for l in range(BOARD_SIZE)]
-            self.board_white = [0 for k in range(BOARD_SIZE)]
-            self.board_black = [0 for k in range(BOARD_SIZE)]
-            self.won_by = 0
-            return
-        # c = copy.deepcopy(parent)
-        # self.__dict__ = c.__dict__
-        # self.turn = self.turn + 1
-        self.turn = parent.turn + 1
-        self.captured = parent.captured[:]
-        self.board_white = parent.board_white[:]
-        self.board_black = parent.board_black[:]
-        self.won_by = parent.won_by
-
-        move_pos = move.position()
-        if self.get_colour(move_pos) > 0:
-            raise IllegalMoveException()
-
-        # TODO: Check for whether this needs to be inverted?
-        my_colour = self.turn % 2 + 1
-        # Place a stone
-        self.set_colour(move_pos, my_colour)
-
-        # Process captures
-        for direction in DIRECTIONS:
-            clrs = self.colours(move_pos, direction, 4)
-            if clrs == [1, 2, 2, 1] or clrs == [2, 1, 1, 2]:
-                capture_pos1 = move_pos.shift(direction, 1)
-                capture_pos2 = move_pos.shift(direction, 2)
-                # Remove stones
-                self.set_colour(capture_pos1, 0)
-                self.set_colour(capture_pos2, 0)
-                # Keep track of capture count
-                self.captured[my_colour] += 1
-
-        # Check for a win (TEMP)
-        for direction in DIRECTIONS:
-            l = 1
-            while l < 5:
-                test_pos = move_pos.shift(direction, l)
-                if test_pos[0] < 0 or \
-                   test_pos[0] >= BOARD_SIZE or \
-                   test_pos[1] < 0 or \
-                   test_pos[1] >= BOARD_SIZE:
-                    # Other end of a potential line is off the edge of the board
-                    break
-                next_col = self.get_colour(test_pos)
-                if next_col != my_colour:
-                    break
-                l += 1
-            m = -1
-            while m > -5:
-                test_pos = move_pos.shift(direction, m)
-                if test_pos[0] < 0 or \
-                   test_pos[0] >= BOARD_SIZE or \
-                   test_pos[1] < 0 or \
-                   test_pos[1] >= BOARD_SIZE:
-                    # Other end of a potential line is off the edge of the board
-                    break
-                next_col = self.get_colour(test_pos)
-                if next_col != my_colour:
-                    break
-                m -= 1
-            total_line_length = 1 + l - m
-            if total_line_length >= 5:
-                self.won_by = my_colour
+        self.state = State(parent.state, move)
 
     def to_move(self):
-        return self.turn % 2
+        return self.state.to_move()
 
     def __repr__(self):
-        edge = "* " * BOARD_SIZE
-        board_array = ["\n" + edge]
-        for y in range(BOARD_SIZE):
-            line = []
-            for x in range(BOARD_SIZE):
-                ind = self.get_colour((x,y))
-                col = " OX"[ind]
-                line.append(col)
-                line.append(' ')
-            board_array.append("".join(line))
-        board_array.append(edge)
-        return "\n".join(board_array)
+        return self.state.__repr__()
 
     def utility(self, player):
-        # 5+ in a row or 5+ captured = infinity
-        if self.captured[0] == 5 or self.won_by == 1:
-            return alpha_beta.infinity
-        if self.captured[1] == 5 or self.won_by == 2:
-            return -alpha_beta.infinity
-        return self.captured[0] - self.captured[1]
-
-
-        # 4+ captured in pente rules
-        # open 4
-        # 2 + open 3s
-        # closed 4
-        # open 3
-        # capture difference? pente rules
-        # occupied positions (centre weighted)
-        # closed 3s
-        # subtract pairs
-        # return 0 # state.utility()
+        return self.state.utility(player)
 
     def score(self):
         return self.utility(None)
@@ -141,8 +44,7 @@ class ABGame():
         # TEMP HACK
         global BOARD_SIZE
         BOARD_SIZE = size
-        self.current_state = state.State()
-        #pdb.set_trace()
+        self.current_state = ABState()
 
     def to_move(self, state=None):
         if state is None:
@@ -156,6 +58,9 @@ class ABGame():
         return state.utility(player)
 
     def successors(self, state):
+        for succ in state.state.successors():
+            yield succ
+        '''
         succ = []
         for x in range(BOARD_SIZE):
             for y in range(BOARD_SIZE):
@@ -166,6 +71,7 @@ class ABGame():
                 except IllegalMoveException:
                     pass
         return succ
+        '''
 
     def terminal_test(self, state):
         return False
