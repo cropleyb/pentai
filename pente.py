6aee3d84fddec3db4bef073acf9c76a8f2dcac46
Bruce Cropley <cropleyb@yahoo.com.au>
2013-11-01 14:12:14 +1100

    Create many pieces

--- pente.py
+++ pente.py
@@ -13,11 +13,12 @@ from os.path import join, dirname
 from kivy.app import App
 from kivy.logger import Logger
 from kivy.uix.scatter import Scatter
+from kivy.uix.widget import Widget
 from kivy.properties import StringProperty
 from kivy.config import Config
 
 
-class Pente(Scatter):
+class Pente(Widget):
     '''Picture is the class that will show the image with a white border and a
     shadow. They are nothing here because almost everything is inside the
     picture.kv. Check the rule named <Picture> inside the file, and you'll see
@@ -27,6 +28,31 @@ class Pente(Scatter):
     '''
 
     source = StringProperty(None)
+
+    def on_touch_down(self, touch):
+        # Logger.info('TOUCH DOWN!')
+        #for filename in ("./images/black.png",):
+        for filename in ("./images/white_transparent.png",):
+            try:
+                # load the image
+                self.piece = Piece(source=filename)
+                self.piece.pos = touch.pos
+                self.add_widget(self.piece)
+            except Exception, e:
+                Logger.exception('Board: Unable to load <%s>' % filename)
+
+    def on_touch_up(self, touch):
+        pass
+        #Logger.info('TOUCH UP!')
+
+    def on_touch_move(self, touch):
+        #Logger.info('TOUCH MOVE!')
+        self.piece.pos = touch.pos
+        # TODO
+        #if touch.x < self.width / 3:
+            #self.player1.center_y = touch.y
+        #if touch.x > self.width - self.width / 3:
+            #self.player2.center_y = touch.y
 
 class Piece(Scatter):
     source = StringProperty(None)
@@ -41,33 +67,16 @@ class PenteApp(App):
 
         # get any files into images directory
         curdir = dirname(__file__)
-        pics = {}
-        brd = None
         for filename in ("./images/board.png",):
             try:
                 # load the image
                 fs = filename[:].strip()
                 picture = Pente(source=filename)
-                brd = picture
-                pics[fs] = picture
 
                 # add to the main field
                 root.add_widget(picture)
             except Exception, e:
                 Logger.exception('Board: Unable to load <%s>' % fs)
-
-        for filename in ("./images/black.png",):
-            try:
-                # load the image
-                fs = filename[:].strip()
-                black = Piece(source=filename)
-                pics[fs] = black
-
-                # add to the board
-                brd.add_widget(black)
-            except Exception, e:
-                Logger.exception('Board: Unable to load <%s>' % fs)
-
 
     def on_pause(self):
         return True
