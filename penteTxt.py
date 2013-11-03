a48653d22ff11bcef76869843792cb47ee681e6f
Bruce Cropley <cropleyb@yahoo.com.au>
2013-11-03 21:49:22 +1100

    Decrufting

--- penteTxt.py
+++ penteTxt.py
@@ -3,21 +3,8 @@
 import alpha_beta
 import ab_bridge
 
-class Move():
-    def __init__(self, pos):
-        self.pos = pos
-    def position(self):
-        return self.pos
+# txtGuiMain.py creates the Game, Players and contains the game turn loop
 
-class Pos():
-    def __init__(self, x, y):
-        self.tup = (x,y)
-    def __getitem__(self, dim):
-        return self.tup[dim]
-    def shift(self, direction, steps):
-        new_pos = (self.tup[0] + (direction[0] * steps), \
-                   self.tup[1] + (direction[1] * steps)) 
-        return Pos(new_pos)
 
 if __name__ == "__main__":
     try:
