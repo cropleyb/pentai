cad7fd3c9a31d7affb1f1ce6026b1b3973f887d8
Bruce Cropley <cropleyb@yahoo.com.au>
2013-11-04 19:01:49 +1100

    more tests

--- t_text_gui.py
+++ t_text_gui.py
@@ -9,7 +9,10 @@ from text_gui import *
 class TextGuiTest(unittest.TestCase):
 
     def setUp(self):
-        rules = Rules(5, "standard")
+        self.setUpWithSize(5)
+
+    def setUpWithSize(self, size):
+        rules = Rules(size, "standard")
         game = Game(rules)
         self.gui = TextGui(game)
 
@@ -46,15 +49,36 @@ class TextGuiTest(unittest.TestCase):
 "2     \n"
 "1     \n")
 
-'''
-- Place stone (pos, colour)
-- Remove stone (pos)
-- add captured stone
-- remove captured stone (for undo/rewind)
-- draw board / flush() (pass for Kivy)
-  - show who is to move (though this isn't a method of the class)
-- get a move
-'''
+    def test_place_one_stone_different_size(self):
+        self.setUpWithSize(7)
+        self.gui.place_stone(4,2,black)
+        game_string = self.gui.board_to_string()
+        self.assertEquals(game_string,
+" abcdefg\n"
+"7       \n"
+"6       \n"
+"5       \n"
+"4       \n"
+"3       \n"
+"2   B   \n"
+"1       \n")
+
+    def test_place_and_remove_one_stone_different_size(self):
+        self.setUpWithSize(7)
+        self.gui.place_stone(4,2,black)
+        self.gui.remove_stone(4,2)
+        game_string = self.gui.board_to_string()
+        self.assertEquals(game_string,
+" abcdefg\n"
+"7       \n"
+"6       \n"
+"5       \n"
+"4       \n"
+"3       \n"
+"2       \n"
+"1       \n")
+
+# TODO: input, player?
 
 if __name__ == "__main__":
     unittest.main()
