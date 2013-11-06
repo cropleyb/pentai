9e512a1e4cf8703d0365e1af8b27f07fd586adf6
Bruce Cropley <cropleyb@yahoo.com.au>
2013-11-06 11:07:57 +1100

    - Board is just what is on the board

--- board.py
+++ board.py
@@ -4,137 +4,27 @@ DIRECTIONS = ((-1,-1),(-1,0),(-1,1),
               (0,-1),(0,0),(0,1),
               (1,-1),(1,0),(1,1))
 
-'''
-class State():
-    """ This is for the state of a game after a particular move. """
-    def __init__(self, parent=None, move=None):
-        self.parent = parent
-        if parent == None:
-            self.turn = 0
-            self.captured = [0,0]
-            # self.board = [[0 for k in range(BOARD_SIZE)] for l in range(BOARD_SIZE)]
-            self.board_white = [0 for k in range(BOARD_SIZE)]
-            self.board_black = [0 for k in range(BOARD_SIZE)]
-            self.won_by = 0
-            return
-        # c = copy.deepcopy(parent)
-        # self.__dict__ = c.__dict__
-        # self.turn = self.turn + 1
-        self.turn = parent.turn + 1
-        self.captured = parent.captured[:]
-        self.board_white = parent.board_white[:]
-        self.board_black = parent.board_black[:]
-        self.won_by = parent.won_by
+class Pos():
+    def __init__(self, x, y):
+        self.tup = (x,y)
+    def __getitem__(self, dim):
+        return self.tup[dim]
+    def shift(self, direction, steps):
+        new_pos = (self.tup[0] + (direction[0] * steps), \
+                   self.tup[1] + (direction[1] * steps)) 
+        return Pos(*new_pos)
 
-        move_pos = move.position()
-        if self.get_colour(move_pos) > 0:
-            raise IllegalMoveException()
-
-        # TODO: Check for whether this needs to be inverted?
-        my_colour = self.turn % 2 + 1
-        # Place a stone
-        self.set_colour(move_pos, my_colour)
-
-        # Process captures
-        for direction in DIRECTIONS:
-            clrs = self.colours(move_pos, direction, 4)
-            if clrs == [1, 2, 2, 1] or clrs == [2, 1, 1, 2]:
-                capture_pos1 = move_pos.shift(direction, 1)
-                capture_pos2 = move_pos.shift(direction, 2)
-                # Remove stones
-                self.set_colour(capture_pos1, 0)
-                self.set_colour(capture_pos2, 0)
-                # Keep track of capture count
-                self.captured[my_colour] += 1
-
-        # Check for a win (TEMP)
-        for direction in DIRECTIONS:
-            l = 1
-            while l < 5:
-                test_pos = move_pos.shift(direction, l)
-                if test_pos[0] < 0 or \
-                   test_pos[0] >= BOARD_SIZE or \
-                   test_pos[1] < 0 or \
-                   test_pos[1] >= BOARD_SIZE:
-                    # Other end of a potential line is off the edge of the board
-                    break
-                next_col = self.get_colour(test_pos)
-                if next_col != my_colour:
-                    break
-                l += 1
-            m = -1
-            while m > -5:
-                test_pos = move_pos.shift(direction, m)
-                if test_pos[0] < 0 or \
-                   test_pos[0] >= BOARD_SIZE or \
-                   test_pos[1] < 0 or \
-                   test_pos[1] >= BOARD_SIZE:
-                    # Other end of a potential line is off the edge of the board
-                    break
-                next_col = self.get_colour(test_pos)
-                if next_col != my_colour:
-                    break
-                m -= 1
-            total_line_length = 1 + l - m
-            if total_line_length >= 5:
-                self.won_by = my_colour
-
-    def to_move(self):
-        return self.turn % 2
-
-    def __repr__(self):
-        edge = "* " * BOARD_SIZE
-        board_array = ["\n" + edge]
-        for y in range(BOARD_SIZE):
-            line = []
-            for x in range(BOARD_SIZE):
-                ind = self.get_colour((x,y))
-                col = " OX"[ind]
-                line.append(col)
-                line.append(' ')
-            board_array.append("".join(line))
-        board_array.append(edge)
-        return "\n".join(board_array)
-
-    def successors(self, state):
-        succ = []
-        for x in range(BOARD_SIZE):
-            for y in range(BOARD_SIZE):
-                pos = (x, y)
-                action = Move(pos)
-                try:
-                    succ.append((action, state.State(state, action)))
-                except IllegalMoveException:
-                    pass
-        return succ
-
-    def utility(self, player):
-        # 5+ in a row or 5+ captured = infinity
-        if self.captured[0] == 5 or self.won_by == 1:
-            return alpha_beta.infinity
-        if self.captured[1] == 5 or self.won_by == 2:
-            return -alpha_beta.infinity
-        return self.captured[0] - self.captured[1]
-
-
-        # 4+ captured in pente rules
-        # open 4
-        # 2 + open 3s
-        # closed 4
-        # open 3
-        # capture difference? pente rules
-        # occupied positions (centre weighted)
-        # closed 3s
-        # subtract pairs
-        # return 0 # state.utility()
-'''
+EMPTY = 0
+BLACK = 1
+WHITE = 2
 
 class Board():
     def __init__(self, size):
         self.size = size
+        self.board_white = [0 for k in range(size)]
+        self.board_black = [0 for k in range(size)]
 
-    '''
-    def get_colour(self, pos):
+    def get_occ(self, pos):
         # return self.board[pos[0]][pos[1]]
         y = pos[1]
         x_pos_bit = 1 << pos[0]
@@ -142,7 +32,7 @@ class Board():
         colour = colour or (self.board_white[y] & x_pos_bit) and 2
         return colour
 
-    def set_colour(self, pos, colour):
+    def set_occ(self, pos, colour):
         # self.board[pos[0]][pos[1]] = colour
         y = pos[1]
         x_pos_bit = 1 << pos[0]
@@ -154,14 +44,6 @@ class Board():
             # clear
             self.board_black[y] &= ~x_pos_bit
             self.board_white[y] &= ~x_pos_bit
-    '''
-
-    def score(self):
-        # TODO
-        return 0
-
-    def make_move(self, x, y):
-        pass # TODO
 
     # TODO - use yield, rename, combine L/R strands, reorder the left strand
     def colours(self, move_pos, direction, length):
--- t_board.py
+++ t_board.py
@@ -3,26 +3,25 @@
 import unittest
 
 from board import *
-from update_substrips import *
-
 
 class BoardTest(unittest.TestCase):
     def test_create_board(self):
         board = Board(size = 13)
         self.assertEquals(board.size, 13)
 
-    def test_empty_board_score_stats(self):
+    def test_empty_board_is_empty(self):
         board = Board(size = 13)
-        self.assertEquals(board.score(), 0)
+        self.assertEquals(board.get_occ(Pos(1, 5)), EMPTY)
 
-    def test_empty_board_place_one_piece(self):
-        self.us_counter = LengthCounter()
-        self.them_counter = LengthCounter()
-
+    def test_empty_board_place_one_black_piece(self):
         board = Board(size = 13)
-        #pdb.set_trace()
-        board.make_move(7,7)
-        #self.assertEquals() TODO
+        board.set_occ(Pos(0, 1), BLACK)
+        self.assertEquals(board.get_occ(Pos(0, 1)), BLACK)
+
+    def test_empty_board_place_one_white_piece(self):
+        board = Board(size = 7)
+        board.set_occ(Pos(2, 3), WHITE)
+        self.assertEquals(board.get_occ(Pos(2, 3)), WHITE)
 
 if __name__ == "__main__":
     unittest.main()
