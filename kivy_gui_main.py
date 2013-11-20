e3167b8431b76e62c59946d16f6ccf524221718c
Bruce Cropley <cropleyb@yahoo.com.au>
2013-11-20 22:09:04 +1100

    Display the board grid

--- kivy_gui_main.py
+++ kivy_gui_main.py
@@ -15,6 +15,8 @@ from kivy.uix.scatter import Scatter
 from kivy.uix.widget import Widget
 from kivy.properties import StringProperty
 from kivy.config import Config
+from kivy.graphics import *
+from kivy.clock import *
 
 class InfoPanel(Widget):
     source = StringProperty(None)
@@ -51,23 +53,45 @@ GridLayout:
 class Pente(Widget):
     pass
 
-BOARD_SIZE = 13
+BOARD_SIZE = 9
+white_filename = "./images/white_transparent.png"
+black_filename = "./images/black_transparent.png"
 
 class Board(Widget):
     source = StringProperty(None)
 
+    def set_up_grid(self, _dt):
+        size_x = self.size[0]
+        size_y = self.size[1]
+
+        with self.canvas:
+            # Black grid lines
+            Color(0, 0, 0)
+            grid_size_x = float(size_x) / (BOARD_SIZE + 1)
+            grid_size_y = float(size_y) / (BOARD_SIZE + 1)
+            # horizontal lines
+            for y in range(1,BOARD_SIZE+1):
+                Rectangle(pos=(grid_size_x-1, grid_size_y*y-1), \
+                          size=(grid_size_x * (BOARD_SIZE-1), 3))
+            # vertical lines
+            for x in range(1,BOARD_SIZE+1):
+                Rectangle(pos=(grid_size_x*x-1, grid_size_y-1), \
+                          size=(3, grid_size_y * (BOARD_SIZE-1)))
+
     def snap_to_grid(self, pos):
-        BS = BOARD_SIZE + 1
-        print "%s, %s" % (pos, self.size)
+        GRID = BOARD_SIZE + 1
+        # print "%s, %s" % (pos, self.size)
         # (367.0, 193.0), [800, 600]
         # 367 / 800
 
-        snapped_x = round(pos[0] / self.size[0] * BS) * self.size[0] / BS
-        snapped_y = round(pos[1] / self.size[1] * BS) * self.size[1] / BS
+        size_x = self.size[0]
+        size_y = self.size[1]
+        snapped_x = round(pos[0] / size_x * GRID) * size_x / GRID
+        snapped_y = round(pos[1] / size_y * GRID) * size_y / GRID
         return snapped_x, snapped_y
 
     def on_touch_down(self, touch):
-        for filename in ("./images/white_transparent.png",):
+        for filename in (white_filename,):
             try:
                 # load the image
                 self.piece = Piece(source=filename)
@@ -98,14 +122,15 @@ class PenteApp(App):
 
         try:
             # load the image
-            filename = "./images/board.png"
-            fs = filename[:].strip()
-            picture = Board(source=filename)
+            board = Board()
 
             # add to the main field
-            root.add_widget(picture)
+            root.add_widget(board)
+
         except Exception, e:
             Logger.exception('Board: Unable to load <%s>' % fs)
+
+        Clock.schedule_once(board.set_up_grid, 1)
 
     def on_pause(self):
         return True
