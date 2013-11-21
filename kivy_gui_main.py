55d480b9b2103dedf0402a08068f4dbd4e948a97
Bruce Cropley <cropleyb@yahoo.com.au>
2013-11-22 08:01:09 +1100

    Added Marker, place and remove black and white stones

--- kivy_gui_main.py
+++ kivy_gui_main.py
@@ -53,12 +53,23 @@ GridLayout:
 class Pente(Widget):
     pass
 
-BOARD_SIZE = 9
-white_filename = "./images/white_transparent.png"
 black_filename = "./images/black_transparent.png"
+white_filename = "./images/white_transparent.png"
+x_filename = "./images/X_transparent.png"
 
 class Board(Widget):
     source = StringProperty(None)
+
+    def __init__(self, *args, **kwargs):
+        self.marker = None
+        self.move_number = 0
+        self.BOARD_SIZE = 9
+
+        # The Grid on the screen allows extra space at the edges
+        self.GRID_SIZE = self.BOARD_SIZE + 1
+
+        self.stones_by_board_pos = {}
+        super(Board, self).__init__(*args, **kwargs)
 
     def set_up_grid(self, _dt):
         size_x = self.size[0]
@@ -67,44 +78,82 @@ class Board(Widget):
         with self.canvas:
             # Black grid lines
             Color(0, 0, 0)
-            grid_size_x = float(size_x) / (BOARD_SIZE + 1)
-            grid_size_y = float(size_y) / (BOARD_SIZE + 1)
+            grid_size_x = float(size_x) / (self.GRID_SIZE)
+            grid_size_y = float(size_y) / (self.GRID_SIZE)
             # horizontal lines
-            for y in range(1,BOARD_SIZE+1):
+            for y in range(1,self.GRID_SIZE):
                 Rectangle(pos=(grid_size_x-1, grid_size_y*y-1), \
-                          size=(grid_size_x * (BOARD_SIZE-1), 3))
+                          size=(grid_size_x * (self.GRID_SIZE-2), 3))
             # vertical lines
-            for x in range(1,BOARD_SIZE+1):
+            for x in range(1,self.GRID_SIZE):
                 Rectangle(pos=(grid_size_x*x-1, grid_size_y-1), \
-                          size=(3, grid_size_y * (BOARD_SIZE-1)))
+                          size=(3, grid_size_y * (self.GRID_SIZE-2)))
 
-    def snap_to_grid(self, pos):
-        GRID = BOARD_SIZE + 1
-        # print "%s, %s" % (pos, self.size)
-        # (367.0, 193.0), [800, 600]
-        # 367 / 800
+    def snap_to_grid(self, screen_pos):
+        return self.board_to_screen(self.screen_to_board(screen_pos))
 
-        size_x = self.size[0]
-        size_y = self.size[1]
-        snapped_x = round(pos[0] / size_x * GRID) * size_x / GRID
-        snapped_y = round(pos[1] / size_y * GRID) * size_y / GRID
-        return snapped_x, snapped_y
+    def screen_to_board(self, screen_pos):
+        """ Convert a screen position (in pixels) to a board coordinate pair,
+            dependant on the size of the board """
+        size_x, size_y = self.size
+        GS = self.GRID_SIZE
+        board_x = round(GS * screen_pos[0] / size_x) - 1
+        board_y = round(GS * screen_pos[1] / size_y) - 1
+        return board_x, board_y
+
+    def board_to_screen(self, board_pos):
+        """ Convert a board coordinate pair to a screen position (in pixels),
+            dependant on the size of the board """
+        size_x, size_y = self.size
+        GS = self.GRID_SIZE
+        screen_x = ((board_pos[0] + 1) / GS) * size_x
+        screen_y = ((board_pos[1] + 1) / GS) * size_y
+        return screen_x, screen_y
 
     def on_touch_down(self, touch):
-        for filename in (white_filename,):
+        # Place a marker at the (snapped) cursor position.
+        if self.marker == None:
             try:
                 # load the image
-                self.piece = Piece(source=filename)
-                self.piece.pos = self.snap_to_grid(touch.pos)
-                self.add_widget(self.piece)
+                # TODO: Separate class?
+                self.marker = Piece(source=x_filename)
             except Exception, e:
-                Logger.exception('Board: Unable to load <%s>' % filename)
+                Logger.exception('Board: Unable to load <%s>' % x_filename)
+        self.marker.pos = self.snap_to_grid(touch.pos)
+        self.add_widget(self.marker)
 
     def on_touch_up(self, touch):
-        pass
+        # If there is an active marker:
+        # Replace the marker to a piece of the appropriate colour
+        # TODO: make the move with the appropriate board position
+        if self.marker != None:
+            self.remove_widget(self.marker)
+            # Quick hack to get both coloured stones on the board
+            board_pos = self.screen_to_board(touch.pos)
+
+            if self.stones_by_board_pos.has_key(board_pos):
+                # There is a piece there already, remove it.
+                current_piece = self.stones_by_board_pos[board_pos]
+                self.remove_widget(current_piece)
+            else:
+                # Nothing there yet, place a stone
+                to_move = self.move_number % 2
+                filename = [white_filename, black_filename][to_move]
+                self.move_number += 1
+                try:
+                    # load the image
+                    new_piece = Piece(source=filename)
+                    self.stones_by_board_pos[board_pos] = new_piece
+                    new_piece.pos = self.board_to_screen(board_pos)
+                    #self.new_piece.pos = self.snap_to_grid(touch.pos)
+                    self.add_widget(new_piece)
+                except Exception, e:
+                    Logger.exception('Board: Unable to load <%s>' % filename)
 
     def on_touch_move(self, touch):
-        self.piece.pos = self.snap_to_grid(touch.pos)
+        # Move the marker position
+        if self.marker != None:
+            self.marker.pos = self.snap_to_grid(touch.pos)
 
 class Piece(Scatter):
     source = StringProperty(None)
@@ -120,15 +169,15 @@ class PenteApp(App):
         # get any files into images directory
         curdir = dirname(__file__)
 
-        try:
-            # load the image
-            board = Board()
+        #try:
+        # load the image
+        board = Board()
 
-            # add to the main field
-            root.add_widget(board)
+        # add to the main field
+        root.add_widget(board)
 
-        except Exception, e:
-            Logger.exception('Board: Unable to load <%s>' % fs)
+        #except Exception, e:
+            #Logger.exception('Board: Unable to load <%s>' % fs)
 
         Clock.schedule_once(board.set_up_grid, 1)
 
@@ -141,5 +190,7 @@ if __name__ == '__main__':
     Config.set('kivy', 'log_enable', 1)
     Config.set('kivy', 'log_dir', 'logs')
     Config.set('kivy', 'log_name', 'kivy_%y-%m-%d_%_.txt')
+    Config.set('graphics', 'width', '600')
+    Config.set('graphics', 'height', '600')
     PenteApp().run()
 
