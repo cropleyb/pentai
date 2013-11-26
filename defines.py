33ebd400e7feb20760ed5e0669d974817ad4f712
Bruce Cropley <cropleyb@yahoo.com.au>
2013-11-26 14:12:19 +1100

    Added opposite_colour() - unused so far.

--- defines.py
+++ defines.py
@@ -5,3 +5,9 @@ DIRECTIONS = ((-1,-1),(-1,0),(-1,1),
 EMPTY = 0
 BLACK = 1
 WHITE = 2
+
+def opposite_colour(col):
+    if col == BLACK:
+        return WHITE
+    if col == WHITE:
+        return BLACK
