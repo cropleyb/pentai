38e3552ba764ac09e3eda3d9f5bdd0f9476b04e5
Bruce Cropley <cropleyb@yahoo.com.au>
2013-12-12 19:31:59 +1100

    Clean up, add 2 tests

--- t_priority_filter.py
+++ t_priority_filter.py
@@ -10,27 +10,30 @@ class PriorityFilterTest(unittest.TestCase):
     def setUp(self):
         self.pf = PriorityFilter()
 
+    def arc(self, *args, **kwargs):
+        self.pf.add_or_remove_candidates(*args, **kwargs)
+
     def test_dont_start_in_the_middle_13(self):
         l = list(self.pf.get_iter(BLACK))
         self.assertEquals(len(l), 0)
 
     def test_iterate_over_one_four(self):
-        self.pf.add_or_remove_candidates(BLACK, 4, ((3,4),))
+        self.arc(BLACK, 4, ((3,4),))
         l = list(self.pf.get_iter(BLACK))
         self.assertEquals(len(l), 1)
         self.assertEquals(l[0],(3,4))
 
     def test_iterate_over_own_colour_first(self):
-        self.pf.add_or_remove_candidates(WHITE, 4, ((1,5),))
-        self.pf.add_or_remove_candidates(BLACK, 4, ((3,4),))
+        self.arc(WHITE, 4, ((1,5),))
+        self.arc(BLACK, 4, ((3,4),))
         l = list(self.pf.get_iter(BLACK))
         self.assertEquals(len(l), 2)
         self.assertEquals(l[0],(3,4))
         self.assertEquals(l[1],(1,5))
 
     def test_iterate_over_higher_priority_first(self):
-        self.pf.add_or_remove_candidates(WHITE, 3, ((1,5),))
-        self.pf.add_or_remove_candidates(WHITE, 4, ((3,4),))
+        self.arc(WHITE, 3, ((1,5),))
+        self.arc(WHITE, 4, ((3,4),))
         l = list(self.pf.get_iter(BLACK))
         self.assertEquals(len(l), 2)
         self.assertEquals(l[0],(3,4))
@@ -52,14 +55,14 @@ class PriorityFilterTest(unittest.TestCase):
 
     def test_iterate_over_other_players_four_before_our_capture(self):
         self.pf.add_or_remove_take(WHITE, (7,2))
-        self.pf.add_or_remove_candidates(BLACK, 4, ((3,4),))
+        self.arc(BLACK, 4, ((3,4),))
         l = list(self.pf.get_iter(WHITE))
         self.assertEquals(len(l), 2)
         self.assertEquals(l[0],(3,4))
         self.assertEquals(l[1],(7,2))
 
     def test_iterate_over_other_players_capture_before_our_threes(self):
-        self.pf.add_or_remove_candidates(BLACK, 3, ((3,4),(1,5)))
+        self.arc(BLACK, 3, ((3,4),(1,5)))
         self.pf.add_or_remove_take(WHITE, (7,2))
         l = list(self.pf.get_iter(WHITE))
         self.assertEquals(len(l), 3)
@@ -69,9 +72,9 @@ class PriorityFilterTest(unittest.TestCase):
         self.assertIn(l[2], our_threes)
 
     def test_iterate_capture_three_and_four_triple_once(self):
-        self.pf.add_or_remove_candidates(WHITE, 3, ((1,5),(2,4)))
+        self.arc(WHITE, 3, ((1,5),(2,4)))
         self.pf.add_or_remove_take(BLACK, (1,5))
-        self.pf.add_or_remove_candidates(BLACK, 4, ((2,4),))
+        self.arc(BLACK, 4, ((2,4),))
         l = list(self.pf.get_iter(WHITE))
         self.assertEquals(len(l), 2)
         self.assertEquals(l[0],(2,4))
@@ -84,7 +87,7 @@ class PriorityFilterTest(unittest.TestCase):
         self.assertEquals(l[0],(1,5))
 
     def test_iterate_over_their_capture_before_our_two(self):
-        self.pf.add_or_remove_candidates(BLACK, 2, ((2,4),(4,6),(5,7)))
+        self.arc(BLACK, 2, ((2,4),(4,6),(5,7)))
         self.pf.add_or_remove_take(WHITE, (1,5))
         l = list(self.pf.get_iter(BLACK))
         self.assertEquals(len(l), 4)
@@ -95,7 +98,7 @@ class PriorityFilterTest(unittest.TestCase):
         self.assertIn(l[3], twos)
 
     def test_iterate_over_their_three_before_our_threat(self):
-        self.pf.add_or_remove_candidates(BLACK, 3, ((2,4),(4,6),))
+        self.arc(BLACK, 3, ((2,4),(4,6),))
         self.pf.add_or_remove_threat(WHITE, (1,5))
         l = list(self.pf.get_iter(BLACK))
         self.assertEquals(len(l), 3)
@@ -105,9 +108,9 @@ class PriorityFilterTest(unittest.TestCase):
         self.assertEquals(l[2],(1,5))
         
     def test_add_and_remove_length_candidate(self):
-        self.pf.add_or_remove_candidates(BLACK, 3, ((2,4),(4,6),), inc=1)
+        self.arc(BLACK, 3, ((2,4),(4,6),), inc=1)
         self.pf.add_or_remove_threat(BLACK, (1,5))
-        self.pf.add_or_remove_candidates(BLACK, 3, ((2,4),(4,6),), inc=-1)
+        self.arc(BLACK, 3, ((2,4),(4,6),), inc=-1)
         l = list(self.pf.get_iter(BLACK))
         self.assertEquals(len(l), 1)
         self.assertEquals(l[0],(1,5))
@@ -125,9 +128,9 @@ class PriorityFilterTest(unittest.TestCase):
         self.assertEquals(len(l), 0)
 
     def test_add_and_remove_length_candidate_from_diff_directions(self):
-        self.pf.add_or_remove_candidates(BLACK, 3, ((2,4),(4,6),), inc=1)
-        self.pf.add_or_remove_candidates(BLACK, 3, ((2,4),(3,3),), inc=1)
-        self.pf.add_or_remove_candidates(BLACK, 3, ((2,4),(4,6),), inc=-1)
+        self.arc(BLACK, 3, ((2,4),(4,6),), inc=1)
+        self.arc(BLACK, 3, ((2,4),(3,3),), inc=1)
+        self.arc(BLACK, 3, ((2,4),(4,6),), inc=-1)
         l = list(self.pf.get_iter(BLACK))
         self.assertEquals(len(l), 2)
         pair = ((2,4),(3,3),)
@@ -135,8 +138,8 @@ class PriorityFilterTest(unittest.TestCase):
         self.assertIn(l[1], pair)
 
     def test_multiple_entries_searched_first(self):
-        self.pf.add_or_remove_candidates(BLACK, 3, ((2,4),(4,6),), inc=1)
-        self.pf.add_or_remove_candidates(BLACK, 3, ((2,4),(3,3),), inc=1)
+        self.arc(BLACK, 3, ((2,4),(4,6),), inc=1)
+        self.arc(BLACK, 3, ((2,4),(3,3),), inc=1)
         l = list(self.pf.get_iter(BLACK))
         self.assertEquals(len(l), 3)
         self.assertEquals(l[0],(2,4))
@@ -145,20 +148,41 @@ class PriorityFilterTest(unittest.TestCase):
         self.assertIn(l[2], others)
 
     def test_min_priority(self):
-        self.pf.add_or_remove_candidates(BLACK, 3, ((2,4),(3,3),), inc=1)
-        self.pf.add_or_remove_candidates(BLACK, 4, ((3,3),), inc=1)
+        self.arc(BLACK, 3, ((2,4),(3,3),), inc=1)
+        self.arc(BLACK, 4, ((3,3),), inc=1)
         l = list(self.pf.get_iter(BLACK, min_priority=4))
         self.assertEquals(len(l), 1)
         self.assertEquals(l[0],(3,3))
 
-    def test_copy(self):
-        self.pf.add_or_remove_candidates(BLACK, 3, ((2,4),(3,3),), inc=1)
-        self.pf.add_or_remove_candidates(BLACK, 4, ((3,3),), inc=1)
+    def test_copy_only_copies_high_priority(self):
+        self.arc(BLACK, 3, ((2,4),(3,3),), inc=1)
+        self.arc(BLACK, 4, ((3,3),), inc=1)
         pfc = self.pf.copy(min_priority=4)
         l = list(pfc.get_iter(BLACK))
         self.assertEquals(len(l), 1)
         self.assertEquals(l[0],(3,3))
 
+    def test_copy_is_deep(self):
+        self.arc(BLACK, 3, ((2,4),(3,3),), inc=1)
+        self.arc(BLACK, 4, ((3,3),), inc=1)
+        pfc = self.pf.copy()
+        pfc.add_or_remove_candidates(BLACK, 4, ((3,3),), inc=-1)
+        # Modifying the descendant should not have affected the parent
+        l = list(self.pf.get_iter(BLACK))
+        self.assertEquals(l[0],(3,3))
+
+    def test_multiple_entries_searched_first2(self):
+        self.arc(BLACK, 3, ((4,6),(5,6),), inc=1)
+        self.arc(BLACK, 3, ((9,6),(10,6),), inc=1)
+        self.arc(BLACK, 3, ((5,6),(9,6),), inc=1)
+        self.arc(WHITE, 2, ((7,8),(8,8),(10,8)), inc=1)
+        self.arc(WHITE, 2, ((8,8),(10,8),(12,8)), inc=1)
+        self.arc(WHITE, 2, ((10,8),(12,8),(13,8)), inc=1)
+        l = list(self.pf.get_iter(BLACK))
+        self.assertEquals(len(l), 9)
+        first_pair = ((5,6), (9,6))
+        self.assertIn(l[0], first_pair)
+        self.assertIn(l[1], first_pair)
 
 if __name__ == "__main__":
     unittest.main()
