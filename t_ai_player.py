d763104a35338b2267883dd4c47ce3f2ae839718
Bruce Cropley <cropleyb@yahoo.com.au>
2013-12-16 20:23:02 +1100

    Add another AI subsystem test

--- t_ai_player.py
+++ t_ai_player.py
@@ -76,14 +76,30 @@ standard rules
 17. (5, 7)
 """
         self.game.load_game(game_str)
-        #pdb.set_trace()
         m = self.p2.do_the_search()
         self.assertEquals(m, (6,7))
-        #print "HI: moved - %s" % (m,)
-        '''
-        ma = p.get_action(self.game, self.gui)
-        self.assertEquals(ma, (4,4))
-        '''
+
+    # !./t_ai_player.py AIPlayerSubsystemTest.test_dont_waste_a_pair
+    def test_dont_waste_a_pair(self):
+        self.p1.set_max_depth(6)
+        game_str = \
+"""Black versus White
+13x13
+standard rules
+1. (6, 6)
+2. (6, 7)
+3. (8, 8)
+4. (5, 5)
+5. (8, 6)
+6. (7, 6)
+7. (8, 5)
+8. (8, 7)
+9. (8, 4)
+10. (5, 8)
+"""
+        self.game.load_game(game_str)
+        m = self.p1.do_the_search()
+        self.assertNotEquals(m, (6,5))
 
 if __name__ == "__main__":
     unittest.main()
