bca187cd9f1f07268833c72c14ff53f9bc5e967d
Bruce Cropley <cropleyb@yahoo.com.au>
2013-12-17 21:28:26 +1100

    Comments only

---
+++ setup.py
@@ -0,0 +1,20 @@
+"""
+Distutils script for building cython .c and .so files. Call it with:
+python setup.py build_ext --inplace
+"""
+
+from distutils.core import setup
+from Cython.Build import cythonize
+
+setup(
+        name = "My hello app",
+        ext_modules = cythonize(
+            'board_strip.pyx',
+            )
+    )
+'''
+# TODO Something broken in distutils? Doesn't like multiple files
+            'take_counter.py',
+            'priority_filter.py',
+            'threat_counter.py'
+'''
