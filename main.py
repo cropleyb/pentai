9e67c34a8c0362fa015d2bbac81d38d9f26070f6
Bruce Cropley <cropleyb@yahoo.com.au>
2014-01-05 22:25:10 +1100

    Main entry point for IOS build must be main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from kivy_gui_main import *
+
+if __name__ == '__main__':
+    Config.set('kivy', 'log_level', 'info')
+    Config.set('kivy', 'log_enable', 1)
+    Config.set('kivy', 'log_dir', 'logs')
+    Config.set('kivy', 'log_name', 'kivy_%y-%m-%d_%_.txt')
+
+    Config.set('graphics', 'width', '457')
+    Config.set('graphics', 'height', '720')
+
+    PenteApp().run()
+
