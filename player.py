462177344e0b1628c4ce27d53f6f2bfb536449be
Bruce Cropley <cropleyb@yahoo.com.au>
2014-01-13 19:59:36 +1100

    key() string for looking up players in a DB

--- player.py
+++ player.py
@@ -6,11 +6,13 @@
 # TODO: Resign
 
 class Player():
-    """ Interface only """
     def __init__(self, name):
         self.name = name
 
     def __repr__(self):
+        return self.name
+
+    def key(self):
         return self.name
 
     def get_name(self):
