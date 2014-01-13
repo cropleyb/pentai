29afad6386a257c6d180d56d74370f356ed6940a
Bruce Cropley <cropleyb@yahoo.com.au>
2014-01-13 20:01:48 +1100

    Getting there

--- t_preserved_game.py
+++ t_preserved_game.py
@@ -7,6 +7,7 @@ from persistent_dict import *
 from priority_filter_2 import *
 from ai_player import *
 from rules import *
+from preserved_game import *
 
 import pdb
 
@@ -16,7 +17,6 @@ class PreservedGameTest(unittest.TestCase):
 
     def create_player(self, name, mmpdl, narrowing, chokes):
         sf = PriorityFilter2()
-        #sf.set_max_moves_per_depth_level(mmpdl=9, narrowing=0, chokes=chokes)
         return AIPlayer(sf, name=name)
 
     def test_preserve_game(self):
@@ -27,7 +27,7 @@ class PreservedGameTest(unittest.TestCase):
         r = Rules(13, "standard")
         orig_game = Game(r, p1, p2)
 
-        pg = orig_game.clone_for_preserving()
+        pg = PreservedGame(orig_game)
         apg = AllPreservedGames("test_games")
         apg.add_game(pg)
         #self.assertEquals(len(l), 0)
