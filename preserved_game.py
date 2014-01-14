7df2c40fb7504ff1db1e6c28d5b3918d98a99f1d
Bruce Cropley <cropleyb@yahoo.com.au>
2014-01-14 11:26:08 +1100

    Convert to/from game

--- preserved_game.py
+++ preserved_game.py
@@ -1,13 +1,28 @@
+from game import *
 from persistent_dict import *
 
 class PreservedGame():
-    def __init__(self, game):
-        self.rules = game.rules # .clone?
+    def __init__(self, game=None):
+        if game:
+            self.rules = game.rules.key()
+            self.date = game.get_date()
+            self.players = [None, game.player[1].key(), game.player[2].key()]
 
-        self.player = [None, game.player[1].key(), game.player[2].key()]
-
-        self.move_history = game.move_history[:]
+            self.winner = game.winner()
+            self.moves = game.move_history[:]
 
     def key(self):
         return "Freddo" # TODO
 
+    def reincarnate(self):
+        orig_game = Game()
+        orig_game.rules = Rules(*self.rules)
+        orig_game.date = self.date
+        orig_game.winner = self.winner
+        orig_game.move_history = self.moves[:]
+
+        # TODO: reincarnate players from player db
+        orig_game.players = self.players[:]
+        #orig_game.players = [None, self.players[1].key(), self.players[2].key()]
+
+        return orig_game
--- t_preserved_game.py
+++ t_preserved_game.py
@@ -3,7 +3,6 @@
 
 import unittest
 from game import *
-from persistent_dict import *
 from priority_filter_2 import *
 from ai_player import *
 from rules import *
@@ -20,18 +19,61 @@ class PreservedGameTest(unittest.TestCase):
         sf = PriorityFilter2()
         return AIPlayer(sf, name=name)
 
+    #    def create_game(self): TODO?
+
     def test_preserve_game(self):
-        chokes = [(4,3),(5,1)]
+        chokes = [] # TODO: Save AI players?!
         p1 = self.create_player("Sonia", 9, 0, chokes)
         p2 = self.create_player("Toby", 9, 0, chokes)
 
         r = Rules(13, "standard")
         orig_game = Game(r, p1, p2)
+        orig_game.make_move((7,3))
 
         pg = PreservedGame(orig_game)
-        apg = GamesDB("test_games.pkl")
-        apg.add(pg)
-        #self.assertEquals(len(l), 0)
+        self.assertEquals(pg.rules, (13, 's'))
+
+        black_player = pg.players[BLACK]
+        self.assertEquals(black_player, "Sonia")
+        white_player = pg.players[WHITE]
+        self.assertEquals(white_player, "Toby")
+
+        today = datetime.date.today()
+        self.assertEquals(pg.date, today)
+
+        self.assertEquals(len(pg.moves), 1)
+        self.assertEquals(pg.moves[0], (7,3))
+
+        self.assertEquals(pg.winner, 0)
+
+    def test_restore_game(self):
+        chokes = [(4,3),(5,1)]
+        pg = PreservedGame()
+        pg.players = [None, "Marjory", "Hubert"]
+
+        pg.rules = (9, 'f')
+        pg.moves = [(8,1)]
+        pg.winner = BLACK
+
+        today = datetime.date.today()
+        pg.date = today
+
+        orig_game = pg.reincarnate()
+
+        self.assertEquals(orig_game.rules.key(), (9, 'f'))
+
+        today = datetime.date.today()
+        self.assertEquals(orig_game.date, today)
+
+        self.assertEquals(len(orig_game.move_history), 1)
+        self.assertEquals(orig_game.move_history[0], (8,1))
+
+        self.assertEquals(orig_game.winner, BLACK)
+
+        black_player = orig_game.players[BLACK]
+        self.assertEquals(black_player, "Marjory")
+        white_player = orig_game.players[WHITE]
+        self.assertEquals(white_player, "Hubert")
 
 if __name__ == "__main__":
     unittest.main()
