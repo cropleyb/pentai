e7739bfddd6c575fc268ae22098305b0df42748e
Bruce Cropley <cropleyb@yahoo.com.au>
2014-01-19 21:52:55 +1100

    Clean up checkbox list a little

--- checkbox_list.py
+++ checkbox_list.py
@@ -1,27 +1,21 @@
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.checkbox import CheckBox
-from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.clock import Clock
 
 from kivy.properties import *
-#from kivy.uix.screenmanager import Screen
-
-import pdb
 
 class CheckBoxList(GridLayout):
     text = StringProperty("")
     group = StringProperty("")
     values = ListProperty([])
-    active = StringProperty("")
-    active_val = StringProperty("")
+    val = StringProperty("")
 
     def __init__(self, *args, **kwargs):
 
         super(CheckBoxList, self).__init__(*args, **kwargs)
 
-        # The properties haven't been initialised you
+        # The properties haven't been initialised yet
         Clock.schedule_once(self.setup, 0)
 
     def setup(self, ignored):
@@ -49,7 +43,6 @@ class CheckBoxList(GridLayout):
     def on_checkbox_active(self, checkbox, value):
         if checkbox.active:
             self.val = checkbox.val
-            self.active_val = checkbox.val
 
 
     def set_active(self, val):
@@ -60,5 +53,4 @@ class CheckBoxList(GridLayout):
         w = self.widgets_by_val[str(val)]
         w.active = True
         self.val = str(val)
-        self.active_val = val # TODO
 
