e41fb25defd09ce3e57ca92e64e77cdf921f720f
Bruce Cropley <cropleyb@yahoo.com.au>
2014-01-21 20:46:06 +1100

    Updated "Menu" Screen to be more of a directory

--- menu_screen.py
+++ menu_screen.py
@@ -1,10 +1,7 @@
 from kivy.uix.screenmanager import Screen
-from kivy.properties import *
 
 class MenuScreen(Screen):
-    show_moves = BooleanProperty(True)
-    show_captures = BooleanProperty(True)
-    require_captures = BooleanProperty(False)
+    """ This is more of a travel agency or a directory than a menu """
 
     def __init__(self, *args, **kwargs):
         super(MenuScreen, self).__init__(*args, **kwargs)
