bf0407914cfa85312d3fde79e83f86d00c2d2235
Bruce Cropley <cropleyb@yahoo.com.au>
2014-01-24 21:16:40 +1100

    Upgrade players DB; show progress

--- upgrade_dbs.py
+++ upgrade_dbs.py
@@ -3,14 +3,28 @@
 import games_mgr as gm_m
 import openings_book as ol_m
 import player as p_m
+import players_mgr as pm_m
 from defines import *
 
-import pdb
+import sys
+import os
+
+def dot():
+    sys.stdout.write('.')
+    sys.stdout.flush()
 
 if __name__ == "__main__":
+    print "Upgrading Players"
+    pm = pm_m.PlayersMgr()
+    for p_id, genome in pm.players.iteritems():
+        if p_id == "max_id":
+            continue
+        sys.stdout.write('.')
+        dot()
+        pm.save(genome)
+
+    print "Upgrading Games"
     gm = gm_m.GamesMgr()
-    #pdb.set_trace()
-    
     to_remove = []
     unknown = p_m.Player("Unknown")
 
@@ -27,6 +41,11 @@ if __name__ == "__main__":
             if game.players[colour] is None:
                 game.players[colour] = unknown
         gm.save(game)
+        dot()
 
     for g_id in to_remove:
+        dot()
         gm.remove_id(g_id)
+
+    # TODO upgrade openings
+
