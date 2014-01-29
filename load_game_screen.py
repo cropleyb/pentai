d57d4015976ca4295e9744eb19234f39965b6df0
Bruce Cropley <cropleyb@yahoo.com.au>
2014-01-29 21:43:02 +1100

    Clean up

--- load_game_screen.py
+++
@@ -1,8 +0,0 @@
-from kivy.uix.screenmanager import Screen
-from kivy.properties import *
-
-class LoadGameScreen(Screen):
-
-    def __init__(self, *args, **kwargs):
-        super(LoadGameScreen, self).__init__(*args, **kwargs)
-
