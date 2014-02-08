47a11053c4c8f69c84b990890b68d54d568ab55c
Bruce Cropley <cropleyb@yahoo.com.au>
2014-02-09 09:12:57 +1100

    Remove more duplication

--- t_all.py
+++ t_all.py
@@ -2,69 +2,49 @@
 
 import unittest
 
-import t_board
-import t_length_lookup_table
-import t_alpha_beta
-import t_ab_state
-import t_rules
-import t_text_gui
-import t_game_state
-import t_utility
-import t_ai_player
-import t_board_strip
-import t_direction_strips
-import t_take_counter
-import t_threat_counter
-import t_priority_filter
-import t_priority_filter_2
-import t_null_filter
-import t_bit_reverse
-import t_game
-import t_vision_filter
-import t_openings_book
-import t_standardise
-import t_preserved_game
-import t_ai_genome
-import t_ai_factory
-import t_players_mgr
-import t_games_mgr
-import t_openings_mover
-import t_misc_db
+import importlib
 
-def add_module(m, all_tests):
-    suite = unittest.defaultTestLoader.loadTestsFromModule(m)
+def iam(m_str):
+    """ import and add a module """
+    global all_tests
+
+    module = importlib.import_module(m_str)
+
+    suite = unittest.defaultTestLoader.loadTestsFromModule(module)
     all_tests.addTest(suite)
 
 def suite():
+    global all_tests
     all_tests = unittest.TestSuite()
-    add_module(t_board, all_tests)
-    add_module(t_length_lookup_table, all_tests)
-    add_module(t_alpha_beta, all_tests)
-    add_module(t_ab_state, all_tests)
-    add_module(t_rules, all_tests)
-    add_module(t_text_gui, all_tests)
-    add_module(t_game_state, all_tests)
-    add_module(t_utility, all_tests)
-    add_module(t_ai_player, all_tests)
-    add_module(t_board_strip, all_tests)
-    add_module(t_direction_strips, all_tests)
-    add_module(t_take_counter, all_tests)
-    add_module(t_priority_filter, all_tests)
-    add_module(t_priority_filter_2, all_tests)
-    add_module(t_null_filter, all_tests)
-    add_module(t_threat_counter, all_tests)
-    add_module(t_game, all_tests)
-    add_module(t_bit_reverse, all_tests)
-    add_module(t_vision_filter, all_tests)
-    add_module(t_openings_book, all_tests)
-    add_module(t_standardise, all_tests)
-    add_module(t_preserved_game, all_tests)
-    add_module(t_ai_genome, all_tests)
-    add_module(t_ai_factory, all_tests)
-    add_module(t_players_mgr, all_tests)
-    add_module(t_games_mgr, all_tests)
-    add_module(t_openings_mover, all_tests)
-    add_module(t_misc_db, all_tests)
+    iam("t_board")
+    iam("t_length_lookup_table")
+    iam("t_alpha_beta")
+    iam("t_ab_state")
+    iam("t_rules")
+    iam("t_text_gui")
+    iam("t_game_state")
+    iam("t_utility")
+    iam("t_ai_player")
+    iam("t_board_strip")
+    iam("t_direction_strips")
+    iam("t_take_counter")
+    iam("t_priority_filter")
+    iam("t_priority_filter_2")
+    iam("t_null_filter")
+    iam("t_threat_counter")
+    iam("t_game")
+    iam("t_bit_reverse")
+    iam("t_vision_filter")
+    iam("t_openings_book")
+    iam("t_standardise")
+    iam("t_preserved_game")
+    iam("t_ai_genome")
+    iam("t_ai_factory")
+    iam("t_players_mgr")
+    iam("t_games_mgr")
+    iam("t_openings_mover")
+    iam("t_misc_db")
+    iam("t_mru_cache")
 
     return all_tests
 
