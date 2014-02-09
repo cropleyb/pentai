70b70c66a2fbe1cc91ce57efea44bffc6414401d
Bruce Cropley <cropleyb@yahoo.com.au>
2014-02-09 13:33:36 +1100

    Allow Creation and name editing of AI players

--- ai_player_screen.py
+++ ai_player_screen.py
@@ -43,35 +43,49 @@ for attr_name, val in GenomeProperties.inst.__dict__.iteritems():
     setattr(GenomeProperties, attr_name, Property(val, allownone=True))
 
 class AIPlayerScreen(Screen):
-    current_name = StringProperty("Choose one")
+    create_text = "Create one"
+    current_name = StringProperty(create_text)
     player_names = ListProperty([])
     genome = GenomeProperties()
+    rename_req = "Give me a name"
+    rename_text = StringProperty(rename_req)
 
     def __init__(self, *args, **kwargs):
         super(AIPlayerScreen, self).__init__(*args, **kwargs)
 
         self.ids.player_spinner_id.bind(text=self.select_player)
+        self.current_name = self.create_text
 
-    # TODO: Does this work?
-    def on_genome(self, *args):
-        pass
-        #print "Genome was updated %s, %s" % args
+        self.ids.rename_id.bind(focus=self.on_rename_focus)
+
+    def on_rename_focus(self, instance, value):
+        if value:
+            # Focus
+            if self.rename_text == self.rename_req:
+                # Create player -> empty the field
+                self.rename_text = ""
 
     def select_player(self, spinner, val):
-        if val == "Create player":
+        if val == self.create_text:
+            self.rename_text = self.rename_req
             return
+        if val != "":
+            self.rename_text = val
         self.edit_player(val)
 
     def edit_player(self, name):
         g = self.pm.find_genome_by_name(name)
         if g is None:
-            st()
             g = self.pm.find_genome_by_name(name)
         self.genome.genome2screen(g)
         self.genome.screen2genome()
 
     def on_enter(self):
-        self.player_names = sorted(self.pm.get_player_names())
+        self.refresh_names()
+
+    def refresh_names(self):
+        self.player_names = [self.create_text]
+        self.player_names.extend(sorted(self.pm.get_player_names()))
 
     def menu(self, unused=None):
         self.app.return_screen()
@@ -80,11 +94,8 @@ class AIPlayerScreen(Screen):
         self.genome.genome2screen()
 
     def save(self, unused=None):
+        self.current_name = self.genome.p_name = self.ids.rename_id.text
         self.genome.screen2genome()
         self.pm.save(self.genome.inst.clone())
-
-    def create_player(self):
-        # TODO
-        pass
-
+        self.refresh_names()
 
