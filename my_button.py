d7dce7291a5f1a1b3c85ad069b81b47cb7f45783
Bruce Cropley <cropleyb@yahoo.com.au>
2014-02-16 14:51:01 +1100

    click sound on button up

--- my_button.py
+++ my_button.py
@@ -3,7 +3,7 @@ from kivy.uix.button import Button
 import audio as a_m
 
 class MyButton(Button):
-    def on_touch_down(self, *args, **kwargs):
+    def on_touch_up(self, *args, **kwargs):
         a_m.instance.click()
-        super(MyButton, self).on_touch_down(*args, **kwargs)
+        super(MyButton, self).on_touch_up(*args, **kwargs)
 
