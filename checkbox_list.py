94f2e60ba3aaf8dafad1a93fba2886af22bca877
Bruce Cropley <cropleyb@yahoo.com.au>
2014-02-22 09:56:52 +1100

    Click on Label to select (or checkbox button)

--- checkbox_list.py
+++ checkbox_list.py
@@ -5,6 +5,20 @@ from kivy.clock import Clock
 
 from kivy.properties import *
 
+class ClickLabel(Label):
+    def __init__(self, *args, **kwargs):
+        self.register_event_type('on_press')
+        return super(ClickLabel, self).__init__(*args, **kwargs)
+
+    def on_touch_down(self, touch):
+        if self.collide_point(touch.x, touch.y):
+            self.dispatch('on_press')
+            return True
+        return super(ClickLabel, self).on_touch_down(touch)
+
+    def on_press(self):
+        pass
+
 class CheckBoxList(GridLayout):
     text = StringProperty("")
     group = StringProperty("")
@@ -12,6 +26,7 @@ class CheckBoxList(GridLayout):
     val = StringProperty("")
 
     def __init__(self, *args, **kwargs):
+        self.cols = 1
 
         super(CheckBoxList, self).__init__(*args, **kwargs)
 
@@ -27,7 +42,8 @@ class CheckBoxList(GridLayout):
 
         first = True
         for v in self.values:
-            l = Label(text=v, halign="left", padding_x=20)
+            l = ClickLabel(text=v)
+            l.bind(on_press=self.label_clicked)
             vals_gl.add_widget(l)
 
             cb = CheckBox(group=self.group, active=first)
@@ -40,10 +56,12 @@ class CheckBoxList(GridLayout):
 
             first = False
 
+    def label_clicked(self, l):
+        self.set_active(l.text)
+
     def on_checkbox_active(self, checkbox, value):
         if checkbox.active:
             self.val = checkbox.val
-
 
     def set_active(self, val):
         """ Set the active value from other python code. """
