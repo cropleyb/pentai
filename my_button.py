24aad169d68170c0cc0383fabbedd3b1936b7343
Bruce Cropley <cropleyb@yahoo.com.au>
2014-02-22 21:01:26 +1100

    Only make click sounds if the button was clicked on

--- my_button.py
+++ my_button.py
@@ -3,10 +3,11 @@ from kivy.uix.button import Button
 import audio as a_m
 
 class MyButton(Button):
-    def on_touch_up(self, *args, **kwargs):
-        if not hasattr(self, "silent"):
-            a_m.instance.click()
-        super(MyButton, self).on_touch_up(*args, **kwargs)
+    def on_touch_up(self, touch, *args, **kwargs):
+        if self.collide_point(*touch.pos):
+            if not hasattr(self, "silent"):
+                a_m.instance.click()
+        super(MyButton, self).on_touch_up(touch, *args, **kwargs)
 
     def sim_press(self):
         self.state = "down"
