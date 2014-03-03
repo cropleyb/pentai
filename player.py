fa0f65b6b216a869cc6f1503e7af51481b570b78
Bruce Cropley <cropleyb@yahoo.com.au>
2014-03-03 18:54:15 +1100

    Remove unused time control support (better in Game)

--- player.py
+++ player.py
@@ -10,8 +10,6 @@
 class Player():
     def __init__(self, p_name):
         self.p_name = p_name
-        self.remaining_time = 0
-        self.total_time = 0
 
     def __repr__(self):
         return self.p_name
@@ -31,16 +29,6 @@ class Player():
             del self.name
         return name
 
-    def get_total_time(self):
-        return self.total_time
-
-    def tick(self, seconds):
-        self.remaining_time -= seconds
-        return self.remaining_time
-
-    def set_remaining_time(self, t):
-        self.remaining_time = t
-
     def prompt_for_action(self, game, gui):
         pass
 
@@ -51,7 +39,5 @@ class Player():
         return 1
 
     def attach_to_game(self, base_game):
-        # TODO: Restore from history
-        self.total_time = base_game.get_rules().time_control
-        self.remaining_time = base_game.get_rules().time_control
+        pass
 
