f2faeda154423795f1ab81ff470bbb9343bb3415
Bruce Cropley <cropleyb@yahoo.com.au>
2014-03-25 10:50:29 +1100

    Use iterator

--- alpha_beta.py
+++ alpha_beta.py
@@ -9,35 +9,34 @@ debug=False
 def argmax(aspi, fn):
     """ aspl: action state pair list
     """
-    # The original list interpretation was faster but didn't short cut wins
-    asp_l = list(aspi)
-    if len(asp_l) == 0:
-        raise NoMovesException()
-    only_one = (len(asp_l) == 1)
-
-    #vals = []
     best = -INFINITY, None
-    for item in asp_l:
-        if only_one:
-            # Don't bother searching, there is only one move
-            # TODO: This is assuming that there is only one move because
-            # they have been correctly prioritised, and that the value
-            # returned by the search is irrelevant.
-            val = 0
-        else:
-            val = fn(item)
-        #vals.append((val, item))
+    try:
+        curr_state = aspi.next()
+    except StopIteration:
+        raise NoMovesException()
+
+    try:
+        next_state = aspi.next()
+    except StopIteration:
+        # Don't bother searching, there is only one move
+        # TODO: This is assuming that there is only one move because
+        # they have been correctly prioritised, and that the value
+        # returned by the search is irrelevant.
+        return curr_state, 0
+
+    val = fn(curr_state)
+    best = val, curr_state
+
+    curr_state = next_state
+    val = fn(curr_state)
+    if val > best[0]:
+        best = val, curr_state
+
+    for curr_state in aspi:
+        val = fn(curr_state)
         if val > best[0]:
-            best = val, item
-    '''
-    if debug:
-        # sort is for debug presentation only. Since this function is only called
-        # once per search, it should not be a problem.
-        print "Deep util"
-        vals.sort()
-        for v in vals:
-            print v
-    '''
+            best = val, curr_state
+
     return best[1], best[0]
 
 def alphabeta_search(state, game):
