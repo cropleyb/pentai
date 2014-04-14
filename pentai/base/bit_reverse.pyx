85b1e7b5bf9f8ea2b8e728cccea33ace8802e105
Bruce Cropley <cropleyb@yahoo.com.au>
2014-04-14 20:00:45 +1000

    Get rid of links

--- pentai/base/bit_reverse.pyx
+++ pentai/base/bit_reverse.pyx
@@ -1,1 +1,19 @@
-bit_reverse.py
+# As per http://stackoverflow.com/questions/12681945/reversing-bits-of-python-integer/20918545#20918545
+
+def reverse_mask(x):
+    x = ((x & 0x55555555) << 1) | ((x & 0xAAAAAAAA) >> 1)
+    x = ((x & 0x33333333) << 2) | ((x & 0xCCCCCCCC) >> 2)
+    x = ((x & 0x0F0F0F0F) << 4) | ((x & 0xF0F0F0F0) >> 4)
+    x = ((x & 0x00FF00FF) << 8) | ((x & 0xFF00FF00) >> 8)
+    x = ((x & 0x0000FFFF) << 16) | ((x & 0xFFFF0000) >> 16)
+    return x
+
+def reverse_mask64(x):
+    #assert(sizeof(x) == 4); // special case: only works for 4 bytes (32 bits).
+    #x = ((x & 0x5555555555555555) << 1) | ((x & 0xAAAAAAAAAAAAAAAA) >> 1)
+    x = ((x & 0x3333333333333333) << 2) | ((x & 0xCCCCCCCCCCCCCCCC) >> 2)
+    x = ((x & 0x0F0F0F0F0F0F0F0F) << 4) | ((x & 0xF0F0F0F0F0F0F0F0) >> 4)
+    x = ((x & 0x00FF00FF00FF00FF) << 8) | ((x & 0xFF00FF00FF00FF00) >> 8)
+    x = ((x & 0x0000FFFF0000FFFF) << 16) | ((x & 0xFFFF0000FFFF0000) >> 16)
+    x = ((x & 0x00000000FFFFFFFF) << 32) | ((x & 0xFFFFFFFF00000000) >> 32)
+    return x
