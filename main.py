11c38b3784cbcc70e05ed097ff1e0a9a68bedc4e
Bruce Cropley <cropleyb@yahoo.com.au>
2014-04-19 10:16:24 +1000

    Do my own scaling

--- main.py
+++ main.py
@@ -6,34 +6,21 @@ from pentai.gui.kivy_gui_main import *
 '''
 
 import pentai.db.persistent_dict as pd_m
+import pentai.gui.scale as sc_m
+
+import kivy.core.window as w_m
 
 import os
+import sys
 
 if __name__ == '__main__':
-    '''
-    import pentai.base.bit_reverse as br_m
-    print "bit_reverse imported!"
-    import pentai.ai.alpha_beta as ab_m
-    print "alpha_beta imported!"
-    import pentai.ai.priority_filter as pf_m
-    print "priority_filter imported!"
-    import pentai.ai.length_lookup_table as llt_m
-    print "llt imported!"
-    import pentai.ai.utility_calculator as uc_m
-    print "uc imported!"
-    import pentai.ai.utility_stats as us_m
-    print "us imported!"
-    import pentai.ai.ab_state as abs_m
-    print "ab_state imported!"
-    '''
-
     Config.set('kivy', 'log_level', 'info')
     Config.set('kivy', 'log_enable', 1)
     Config.set('kivy', 'log_dir', 'logs')
     Config.set('kivy', 'log_name', 'kivy_%y-%m-%d_%_.txt')
 
-    Config.set('graphics', 'width', '457')
-    Config.set('graphics', 'height', '720')
+    f = w_m.Window.size[1] / 720.0
+    sc_m.set_scale_factor(f)
 
     try:
         pentai_path = os.environ['PENTAIPATH']
@@ -48,11 +35,11 @@ if __name__ == '__main__':
         pd_m.base_dir = pentai_path
         
         err_fn = os.path.join(pa.user_data_dir, "err.txt")
+
         try:
             print "Previous crash:"
             with open(err_fn) as err_log:
                 print err_log.readlines()
-            a = c
         except:
             pass
         pa.run()
