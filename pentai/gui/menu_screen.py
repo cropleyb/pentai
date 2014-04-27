9e29cb9288a046e0e284344cf61d17c6deebf932
Bruce Cropley <cropleyb@yahoo.com.au>
2014-04-27 18:50:11 +1000

    Updated Winning Ways text and link.

--- pentai/gui/menu_screen.py
+++ pentai/gui/menu_screen.py
@@ -14,7 +14,7 @@ class MenuScreen(Screen):
 
         self.ids.about_id.ids.label_id.bind(on_ref_press=self.follow_link)
         self.about_text = \
-"""Pente is a strategy board game for two or more players, created in 1977 by Gary Gabrel. It is now owned by Hasbro, and the board game can be bought from [ref=ww][color=00ffff]Winning Ways[/color][/ref]. Or not...
+"""Pente is a strategy board game for two or more players, created in 1977 by Gary Gabrel. It is now owned by Hasbro, and the board game could be bought from [ref=ww][color=00ffff]Winning Ways[/color][/ref] until recently.
 The rules of the game are very simple: The first player to get 5 stones in a straight line wins. Pairs of stones can be captured by placing a stone at each end of the pair. Capturing 5 pairs first also wins. Click "Rules Demo" below!
 
 PentAI is a computer program to play Pente. It can be configured to play in a wide range of ability levels, from a complete beginner to a strong amateur player. For more information about how PentAI works, see my [ref=bc][color=00ffff]website[/color][/ref].
@@ -42,7 +42,7 @@ Bruce
     def follow_link(self, inst, ref):
         print "User clicked on: %s" % ref
         if ref == "ww":
-            link = "http://winning-moves.com/product/Pente.asp"
+            link = "http://winning-moves.com"
         elif ref == "bc":
             link = "http://www.bruce-cropley.com/pente"
         print link
