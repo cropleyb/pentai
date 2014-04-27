0c2ee252eded9b167409519997ab8f59e1436f6b
Bruce Cropley <cropleyb@yahoo.com.au>
2014-04-27 14:22:37 +1000

    More trimming

--- iOS/trim_py_zip.sh
+++ iOS/trim_py_zip.sh
@@ -5,7 +5,7 @@ zip -d python27.zip smtplib.pyo aifc.pyo rfc822.pyo ftplib.pyo mhlib.pyo
 zip -d python27.zip httplib.pyo 
 zip -d python27.zip imaplib.pyo "compiler*" locale.pyo cookielib.pyo optparse.pyo difflib.pyo argparse.pyo
 zip -d python27.zip "*sql*"
-zip -d python27.zip "*plat-mac*"
+zip -d python27.zip "*plat-mac*" "*mail*" "*scripts*"
 
 # ZODB 
 zip -d python27.zip "*tests*" "*.rej" "*.py" "*.pyc" "*.c" "*.so.libs" "*.so.o"
