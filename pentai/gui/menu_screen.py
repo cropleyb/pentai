84f9a83a12f46a93902bff5bd458e219ff0be155
Bruce Cropley <cropleyb@yahoo.com.au>
2014-04-28 13:55:59 +1000

    Workaround for https://github.com/kivy/kivy/issues/2119

--- pentai/gui/menu_screen.py
+++ pentai/gui/menu_screen.py
@@ -7,44 +7,56 @@ from scrollable_label import *
 
 class MenuScreen(Screen):
     """ This is more of a travel agency or a directory than a menu """
-    about_text = StringProperty("")
+    about_text1 = StringProperty("")
+    about_text2 = StringProperty("")
+    about_text3 = StringProperty("")
+    about_text4 = StringProperty("")
+    about_text5 = StringProperty("")
+    about_text6 = StringProperty("")
 
     def __init__(self, *args, **kwargs):
         super(MenuScreen, self).__init__(*args, **kwargs)
 
-        self.ids.about_id.ids.label_id.bind(on_ref_press=self.follow_link)
-        self.about_text = \
+        self.ids.gl_id.ids.label1_id.bind(on_ref_press=self.follow_link)
+        self.ids.gl_id.ids.label2_id.bind(on_ref_press=self.follow_link)
+        self.about_text1 = \
 """Pente is a strategy board game for two or more players, created in 1977 by Gary Gabrel. It is now owned by Hasbro, and the board game could be bought from [ref=ww][color=00ffff]Winning Ways[/color][/ref] until recently.
+
 The rules of the game are very simple: The first player to get 5 stones in a straight line wins. Pairs of stones can be captured by placing a stone at each end of the pair. Capturing 5 pairs first also wins. Click "Rules Demo" below!
-
-PentAI is a computer program to play Pente. It can be configured to play in a wide range of ability levels, from a complete beginner to a strong amateur player. For more information about how PentAI works, see my [ref=bc][color=00ffff]website[/color][/ref].
-
-If you get a sore brain, you can create a game between a couple of Artificial Intelligence (AI) players and watch what they do. Human versus Human games are also possible, though you may prefer to play with a real set.
+"""
+        self.about_text2 = \
+"""PentAI is a computer program to play Pente. It can be configured to play in a wide range of ability levels, from a complete beginner to a strong amateur player. For more information about how PentAI works, see my [ref=bc][color=00ffff]website[/color][/ref].
+"""
+        self.about_text3 = \
+"""If you get a sore brain, you can create a game between a couple of Artificial Intelligence (AI) players and watch what they do. Human versus Human games are also possible, though you may prefer to play with a real set.
 
 Games are automatically saved, and can be resumed if they were left unfinished.
-
-There are a few settings that you might like to change, for things such as move confirmation style, sound volume and so on.
+"""
+        self.about_text4 = \
+"""There are a few settings that you might like to change, for things such as move confirmation style, sound volume and so on.
 
 Thanks go to:
-
-- Alan Morris for the wonderful voiceover
+"""
+        self.about_text5 = \
+"""- Alan Morris for the wonderful voiceover
 - Marion and Noel Lodge for their constant support and feedback
 - Thad Spreg for his expert Pente insights
 - Annette Campbell for inspiring me to write this in the first place
 - Sascha, Jespah and Arwen for their comments on "The Sod"
 - The Kivy team for lots of help with their excellent toolset
 - Lots of other people for their support and feedback
-
+"""
+        self.about_text6 = \
+"""
 Enjoy,
 Bruce
 """
-
     def follow_link(self, inst, ref):
         print "User clicked on: %s" % ref
         if ref == "ww":
             link = "http://winning-moves.com"
         elif ref == "bc":
-            link = "http://www.bruce-cropley.com/pente"
+            link = "http://www.bruce-cropley.com/pentai"
         print link
         webbrowser.open_new_tab(link)
 
--- pentai/gui/scrollable_label.py
+++ pentai/gui/scrollable_label.py
@@ -2,5 +2,15 @@ from kivy.uix.scrollview import *
 from kivy.properties import StringProperty
 
 class ScrollableLabel(ScrollView):
-    text = StringProperty('')
+    text1 = StringProperty('')
+    text2 = StringProperty('')
+    text3 = StringProperty('')
+    text4 = StringProperty('')
+    text5 = StringProperty('')
+    text6 = StringProperty('')
+
+    def __init__(self, *args, **kwargs):
+        super(ScrollableLabel, self).__init__(*args, **kwargs)
+        #gl = self.ids.gl_id
+        #gl.bind(minimum_height=gl.setter('height'))
 
