b0a00ba29b3f3834e3dd5bf5c94eb23cbb5ecf43
Bruce Cropley <cropleyb@yahoo.com.au>
2014-05-04 19:45:35 +1000

    Candidate selection after rotation AND translation

--- pentai/ai/standardise.py
+++ pentai/ai/standardise.py
@@ -1,9 +1,28 @@
 import rot_standardise as rs_m
 import trans_standardise as t_m
 
-def standardise(orig_state):
-    rot_std, fwd, rev = rs_m.standardise(orig_state)
+def standardise(orig_state): # Test code only
+    possibilities = rs_m.rot_possibilities(orig_state)
+    all_combined = []
+    for p in possibilities:
+        c = combine_and_trim(p)
+        all_combined.append((c[0], c))
+
+    try:
+        s = min(all_combined)[1]
+    except IndexError:
+        import pdb
+        pdb.set_trace()
+    return s
+
+def combine_and_trim(poss):
+    rot_std, fwd, rev = poss
+
     both_std, lshift, dshift = t_m.shift(rot_std)
+    trimmed = both_std.get_board().d_strips[0].strips
+    while len(trimmed) and trimmed[-1] == 0:
+        trimmed.pop()
+    trimmed_tuple = tuple(trimmed)
     # Now combine lshift and dshift into fwd and rev
     def both_fwd(*pos):
         x, y = fwd(*pos)
@@ -16,5 +35,5 @@ def standardise(orig_state):
         y += dshift
         new_p = rev(x, y)
         return new_p
-    return both_std, both_fwd, both_rev
+    return trimmed_tuple, both_fwd, both_rev
 
--- pentai/ai/t_standardise.py
+++ pentai/ai/t_standardise.py
@@ -6,21 +6,44 @@ from pentai.base.game_state import *
 from pentai.base.game import *
 from pentai.base.rules import *
 from pentai.ai.standardise import *
+from pentai.base.direction_strips import *
+
 
 class StandardiseTest(unittest.TestCase):
     def setUp(self):
         self.rules = Rules(19, "standard")
         self.game = Game(self.rules, p_m.Player("BC"), p_m.Player("Whoever"))
 
-    def test_flip_and_shift(self): # TODO: rename to flip
-        self.game.load_moves("1. (10,10)\n2. (11,9)")
+    #!python ./pentai/ai/t_standardise.py StandardiseTest.test_just_shift
+    def test_diagonals(self):
+        for i in range(1,19):
+            self.game.load_moves("1. (%s,%s)" % (i,i))
+            std, fwd, rev = standardise(self.game.current_state)
+            std_ii = fwd(i, i)
+            if i < 5:
+                self.assertEquals(std_ii, (i, 18-i))
+            elif i > 13:
+                self.assertEquals(std_ii, (18-i, i))
+            else:
+                self.assertEquals(std_ii, (5, 5))
+
+            self.game.go_to_the_beginning()
+
+    def test_rot_and_shift(self):
+        self.game.load_moves("1. (8,7)\n2. (10,11)")
         std, fwd, rev = standardise(self.game.current_state)
 
-        #brd = std.get_board()
-        self.assertEqual(std.get_all_captured(), [0, 0, 0])
-        print std
-        #self.assertEqual(brd.get_occ((0,8)), BLACK)
+        ds = EDirectionStrips(19)
+        ds._override(std)
 
+        self.assertEqual(ds.get_occ((5,5)), BLACK)
+        self.assertEqual(ds.get_occ((7,9)), WHITE)
+
+    def test_standardisation_trims_array(self):
+        self.game.load_moves("1. (8,7)")
+        std, fwd, rev = standardise(self.game.current_state)
+
+        self.assertEqual(len(std), 6)
 
 if __name__ == "__main__":
     unittest.main()
