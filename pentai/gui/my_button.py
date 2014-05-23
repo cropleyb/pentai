a7028ca3d3dea5a9f8891dfd2947b671bbe02b7e
Bruce Cropley <cropleyb@yahoo.com.au>
2014-05-23 15:12:56 +1000

    Make "silent" an attribute from __init__

--- pentai/gui/my_button.py
+++ pentai/gui/my_button.py
@@ -1,11 +1,16 @@
 
 from kivy.uix.button import Button
 import audio as a_m
+from pentai.base.defines import *
 
 class MyButton(Button):
+    def __init__(self, *args, **kwargs):
+        super(MyButton, self).__init__(*args, **kwargs)
+        self.silent = False
+
     def on_touch_up(self, touch, *args, **kwargs):
         if self.collide_point(*touch.pos):
-            if not hasattr(self, "silent"):
+            if not self.silent:
                 a_m.instance.click()
         super(MyButton, self).on_touch_up(touch, *args, **kwargs)
 
@@ -14,7 +19,7 @@ class MyButton(Button):
 
     def sim_release(self, ignored=None):
         self.state = "normal"
-        if not hasattr(self, "silent"):
+        if not self.silent:
             a_m.instance.click()
 
 
