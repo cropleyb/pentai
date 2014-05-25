c2dcd80a5c214c612db8d95b2471d4ff70b37f7b
Bruce Cropley <cropleyb@yahoo.com.au>
2014-05-25 22:09:06 +1000

    Log with timestamps

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ def main():
     import startup
     startup.run()
 
+import logging, sys
+
 if __name__ == "__main__":
     '''
     import pstats, cProfile
@@ -12,5 +14,11 @@ if __name__ == "__main__":
     s.strip_dirs().sort_stats("cumulative").print_stats(20) # or "time"
     #s.strip_dirs().sort_stats("time").print_stats(20)
     '''
+    formatter = logging.Formatter("[%(asctime)s.%(msecs)03d][%(levelname)s][%(message)s]",
+                                          "%H:%M:%S")
+    console = logging.StreamHandler()
+    console.setFormatter(formatter)
+    sys._kivy_logging_handler = console
+
     main()
 
