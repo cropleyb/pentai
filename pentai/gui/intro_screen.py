79e0d51bbbac4069f651396fde4a28651a69db5e
Bruce Cropley <cropleyb@yahoo.com.au>
2014-05-30 18:31:15 +1000

    More action on the screen ;)

--- pentai/gui/intro_screen.py
+++ pentai/gui/intro_screen.py
@@ -48,7 +48,7 @@ class IntroScreen(Screen):
         if self.progress_bar.value >= 1000:
             self.progress_bar.value = 0
             self.show_activity()
-        Clock.schedule_once(self.add_progress, .2 + 2 * random.random())
+        Clock.schedule_once(self.add_progress, .2 + 1 * random.random())
 
     activity_list = [
         "Unpacking board", "Polishing stones", "Brushing teeth", "Arranging furniture", "Winding clocks", "Getting snacks", "Putting kids to bed", "Putting cat out", "Adjusting central heating", "Sound check", "Getting a drink", "Call of nature", "Preparing distractions", "Answering phone", "Texting workmate", "Putting away dishes", "Reading the paper", "Paying bills", "Gathering audience", "Placing wagers", "Practicing openings", "Gossiping with neighbours", "Procrastinating", "Boasting about previous games", "Anticipating", "Watching an ad", "Watering pot-plants", "Online shopping", "Grazed knee", "Complaining", "None of your business", "Composing oneself", "Nothing much", "CPR on Aunt Mabel", "Finishing meal", "Toasting marshmallows", "Biting nails", "Scratching", "(censored)", "Changing footwear", "You don't want to know", "Attending to personal hygene", "Is it alive?", "Contributing to Global Warming", "Meditating", "Packing"
