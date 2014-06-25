53e7d30f6a6a03dcf46c902a509a2f979d332122
Bruce Cropley <cropleyb@yahoo.com.au>
2014-06-25 15:41:58 +1000

    Move logging setup inside main()

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ sys.exit(0)
 '''
 
 def main():
+    formatter = logging.Formatter("[%(asctime)s.%(msecs)03d][%(levelname)s][%(message)s]",
+                                          "%H:%M:%S")
+    console = logging.StreamHandler()
+    console.setFormatter(formatter)
+    sys._kivy_logging_handler = console
+
     import startup
     startup.run()
 
@@ -20,11 +26,6 @@ if __name__ == "__main__":
     s.strip_dirs().sort_stats("cumulative").print_stats(20) # or "time"
     #s.strip_dirs().sort_stats("time").print_stats(20)
     '''
-    formatter = logging.Formatter("[%(asctime)s.%(msecs)03d][%(levelname)s][%(message)s]",
-                                          "%H:%M:%S")
-    console = logging.StreamHandler()
-    console.setFormatter(formatter)
-    sys._kivy_logging_handler = console
 
     main()
 
