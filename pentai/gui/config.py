1a8bc8b2acd379f713b684df2a4463d0edd04af3
Bruce Cropley <cropleyb@yahoo.com.au>
2014-06-28 18:15:56 +1000

    Config singleton. Probably overkill but hey.

---
+++ pentai/gui/config.py
@@ -0,0 +1,23 @@
+from kivy.config import Config
+from kivy.config import ConfigParser
+import os
+
+def config_instance():
+    return _config
+
+        #ini_file = "pentai.ini"
+
+def create_config_instance(ini_file, user_path):
+    global _config
+
+    ini_path = os.path.join(user_path, ini_file)
+    if not ini_file in os.listdir(user_path):
+    # if True:
+        log.info("Copying ini")
+        import shutil
+        shutil.copy(ini_file, ini_path)
+
+    _config = ConfigParser()
+    _config.read(ini_path)
+
+    return _config
