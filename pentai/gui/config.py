14e87fed6c2b75b9827993c882d372e11c9c8673
Bruce Cropley <cropleyb@yahoo.com.au>
2014-06-30 15:36:50 +1000

    Import our logger for first settup code.

--- pentai/gui/config.py
+++ pentai/gui/config.py
@@ -1,18 +1,19 @@
 from kivy.config import Config
 from kivy.config import ConfigParser
+
+import pentai.base.logger as log
+
 import os
 
 def config_instance():
     return _config
-
-        #ini_file = "pentai.ini"
 
 def create_config_instance(ini_file, user_path):
     global _config
 
     ini_path = os.path.join(user_path, ini_file)
     if not ini_file in os.listdir(user_path):
-    # if True:
+    #if True:
         log.info("Copying ini")
         import shutil
         shutil.copy(ini_file, ini_path)
