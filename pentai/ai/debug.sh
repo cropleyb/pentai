08611ac726735485bf374c092aa300bdab197021
cropleyb <cropleyb@yahoo.com.au>
2014-08-03 10:59:51 +1000

    Utility stats has some cythonisation

--- pentai/ai/debug.sh
+++ pentai/ai/debug.sh
@@ -3,7 +3,7 @@ rm ab_state.so
 rm alpha_beta.so
 rm openings_mover.so
 rm utility_calculator.so
-rm utility_stats.so
+# rm utility_stats.so This has a couple of cython lines in it which can be disabled
 rm priority_filter.so
 rm priority_filter_2.so
 rm priority_filter_3.so
@@ -19,7 +19,7 @@ rm alpha_beta.py
 rm length_lookup_table.py # TEMP
 rm openings_mover.py
 rm utility_calculator.py
-rm utility_stats.py
+# rm utility_stats.py
 rm priority_filter.py
 rm priority_filter_2.py
 rm priority_filter_3.py
@@ -34,7 +34,7 @@ ln -s ab_state.pyx           ab_state.py
 ln -s alpha_beta.pyx         alpha_beta.py
 ln -s openings_mover.pyx     openings_mover.py
 ln -s utility_calculator.pyx utility_calculator.py
-ln -s utility_stats.pyx utility_stats.py
+# ln -s utility_stats.pyx utility_stats.py
 ln -s priority_filter.pyx    priority_filter.py
 ln -s priority_filter_2.pyx  priority_filter_2.py
 ln -s priority_filter_3.pyx  priority_filter_3.py
