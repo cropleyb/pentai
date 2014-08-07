568833d0fb19ea2c89b1f11781c4f590e4fc0833
cropleyb <cropleyb@yahoo.com.au>
2014-08-07 16:12:45 +1000

    No functional change

--- pentai/gui/menu_screen.py
+++ pentai/gui/menu_screen.py
@@ -23,6 +23,7 @@ class MenuScreen(Screen):
 
         self.ids.gl_id.ids.label1_id.bind(on_ref_press=self.follow_link)
         self.ids.gl_id.ids.label2_id.bind(on_ref_press=self.follow_link)
+
         self.about_text1 = \
 """      Pente is a strategy board game for two or more players, created in 1977 by Gary Gabrel. It is now owned by Hasbro, and the board game could be bought from [ref=ww][color=00ffff]Winning Moves[/color][/ref] until recently.
                   (scroll for more)
