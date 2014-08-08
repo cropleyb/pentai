308ec4f76a218005b33a01b3c7be9ec45543fb6c
cropleyb <cropleyb@yahoo.com.au>
2014-08-08 15:21:21 +1000

    Inherit more from player_screen

--- pentai/gui/ai_player_screen.py
+++ pentai/gui/ai_player_screen.py
@@ -4,16 +4,15 @@ from kivy.event import EventDispatcher
 
 import checkbox_list as cb_l
 
+from pentai.gui.player_screen import *
 import pentai.ai.ai_genome as aig_m
-
-
 
 # TODO: Make a reusable class for this.
 class GenomeProperties(EventDispatcher):
     inst = aig_m.AIGenome("Whatever")
 
     def genome2screen(self, g=None):
-        ''' Copy from genome g into self's namespace '''
+        """ Copy from genome g into self's namespace """
         if g is None:
             # Copy from the default genome instance
             g = self.inst
@@ -22,7 +21,7 @@ class GenomeProperties(EventDispatcher):
             setattr(self, k, v)
 
     def screen2genome(self, unused=None):
-        ''' Copy from our namespace into the default global instance '''
+        """ Copy from our namespace into the default global instance """
         for k,v in self.inst.__dict__.iteritems():
             setattr(self.inst, k, getattr(self, k))
         return self.inst
@@ -31,68 +30,27 @@ class GenomeProperties(EventDispatcher):
 for attr_name, val in GenomeProperties.inst.__dict__.iteritems():
     setattr(GenomeProperties, attr_name, Property(val, allownone=True))
 
-class AIPlayerScreen(Screen):
-    create_text = "Create one"
-    current_name = StringProperty(create_text)
-    player_names = ListProperty([])
+class AIPlayerScreen(PlayerScreen):
     genome = GenomeProperties()
-    rename_req = "Give me a name"
-    rename_text = StringProperty(rename_req)
 
-    def __init__(self, *args, **kwargs):
-        super(AIPlayerScreen, self).__init__(*args, **kwargs)
-
-        self.ids.player_spinner_id.bind(text=self.select_player)
-        self.current_name = self.create_text
-
-        self.ids.rename_id.bind(focus=self.on_rename_focus)
-
-    def on_rename_focus(self, instance, value):
-        if value:
-            # Focus
-            if self.rename_text == self.rename_req:
-                # Create player -> empty the field
-                self.rename_text = ""
-
-    def select_player(self, spinner, val):
-        if val == self.create_text:
-            self.rename_text = self.rename_req
-            return
-        if val != "":
-            self.rename_text = val
-        self.edit_player(val)
+    player_class = aig_m.AIGenome
+    player_type_str = "AI"
 
     def edit_player(self, name):
         g = self.pm.find_genome_by_name(name)
-        if g is None:
-            g = self.pm.find_genome_by_name(name)
         self.genome.genome2screen(g)
         self.genome.screen2genome()
 
-    def on_enter(self):
-        self.refresh_names()
+    def update_player(self, player, new_name):
+        self.genome.screen2genome()
+        self.genome.inst.p_name = new_name
+        player.genome.__dict__.update(self.genome.inst.__dict__)
 
-    def refresh_names(self):
-        self.player_names = [self.create_text]
-        pns = self.pm.get_ai_player_names()
-        self.player_names.extend(sorted(pns))
-
-    def menu(self, unused=None):
-        self.app.return_screen()
+    def get_players(self):
+        return self.pm.get_ai_player_names()
 
     def cancel(self, unused=None):
         self.genome.genome2screen()
-
-    def save(self, unused=None):
-        if self.ids.rename_id.text in [self.rename_req, ""]:
-            # Attempt to save with no name set.
-            self.app.display_error("Please choose a name first")
-            return
-        self.genome.p_name = self.ids.rename_id.text
-        self.genome.screen2genome()
-        self.pm.save(self.genome.inst.clone())
-        self.current_name = self.genome.p_name
-        self.refresh_names()
 
     def show_help(self):
         self.app.show_ai_help()
