619765182f73e401632bee506714192e73ce0c01
cropleyb <cropleyb@yahoo.com.au>
2014-08-12 15:17:09 +1000

    Increase recursion limit

--- main.py
+++ main.py
@@ -5,12 +5,15 @@ import sys
 sys.exit(0)
 '''
 
+import sys
+
 def main():
     formatter = logging.Formatter("[%(asctime)s.%(msecs)03d][%(levelname)s][%(message)s]",
                                           "%H:%M:%S")
     console = logging.StreamHandler()
     console.setFormatter(formatter)
     sys._kivy_logging_handler = console
+    sys.setrecursionlimit(500)
 
     logging.getLogger("ZODB.FileStorage").addHandler(console)
     logging.getLogger("ZODB.lock_file").addHandler(console)
