d74b2a1336c8de761acb9bb68c2b67338c1d27d5
cropleyb <cropleyb@yahoo.com.au>
2014-08-16 11:28:15 +1000

    Dependencies for help screen in .kv

--- pentai/gui/help_screen.py
+++ pentai/gui/help_screen.py
@@ -1,5 +1,8 @@
 from kivy.uix.screenmanager import Screen
 
+from pentai.gui.spacer import *
+from pentai.gui.scrollable_label import *
+
 class HelpScreen(Screen):
     def on_pre_enter(self):
         self.set_text()
