3cbe0141f749bbe6666b3e14d868b76f0ece0cdf
cropleyb <cropleyb@yahoo.com.au>
2014-08-18 16:05:18 +1000

    ...

--- pentai/gui/help_screen.py
+++ pentai/gui/help_screen.py
@@ -2,6 +2,7 @@ from kivy.uix.screenmanager import Screen
 
 from pentai.gui.spacer import *
 from pentai.gui.scrollable_label import *
+from pentai.gui.section import *
 
 class HelpScreen(Screen):
     def on_pre_enter(self):
