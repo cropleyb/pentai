f8404c5a68e17e770d14fb5b946bfcdf8dff1e75
cropleyb <cropleyb@yahoo.com.au>
2014-08-19 15:51:03 +1000

    Convert AI help to sections

--- pentai/gui/ai_help_screen.py
+++ pentai/gui/ai_help_screen.py
@@ -1,30 +1,42 @@
 import help_screen as hs_m
 
 from pentai.gui.fonts import AI_FONT
+from kivy.properties import StringProperty
 
-class AIHelpScreen(hs_m.HelpScreen):
+
+class AIHelpText(hs_m.HelpText):
+    intro_text = StringProperty("")
+    properties_text = StringProperty("")
+    openings_text = StringProperty("")
+    profiles_text= StringProperty("")
 
     def __init__(self, *args, **kwargs):
         self.heading = "AI Player Help"
-        super(AIHelpScreen, self).__init__(*args, **kwargs)
+        self.set_text()
+        super(AIHelpText, self).__init__(*args, **kwargs)
 
     def set_text(self):
-        sc = self.ids.scrollable_id
-        sc.text1 = """ You can either edit an existing player by selecting their name (choose under "Create One"), or create a new one (leave the top box with "Create One", and type in the name immediately below).
+        self.intro_text = """ You can either edit an existing player by selecting their name (choose under "Create One"), or create a new one (leave the top box with "Create One", and type in the name immediately below).
+"""
 
- There are several properties that can be adjusted for a given [font=%s]Artificial[/font]
- [font=%s]Intelligence[/font] (AI) player profile:
+        self.properties_text = """ There are several properties that can be adjusted for a given [font=%s]Artificial[/font] [font=%s]Intelligence[/font] (AI) player profile:
 
  [i]Depth[/i]: Controls how many turns the AI looks ahead for each move.
  [i]Vision[/i]: How often does the AI see the better move possibilities?
  [i]Openings book[/i]: Should the AI be able to refer to previous games?
- [i]Judgement[/i]: How well should the AI judge the value of positions?""" % (AI_FONT, AI_FONT)
+ [i]Judgement[/i]: How well should the AI judge the value of positions?
+ """ % (AI_FONT, AI_FONT)
 
-        sc.text2 = """
- The Openings Book uses positions that have been played previously with PentAI, and by many skilled players on pente.org. Every time you play a game against an AI Player, the result is added to the Openings Book, along with the first 12 or so moves. (unless you disable this in [i]Settings[/i])
+        self.openings_text = """ The Openings Book uses positions that have been played previously with PentAI, and by many skilled players on pente.org. Every time you play a game against an AI Player, the result is added to the Openings Book, along with the first 12 or so moves. (unless you disable this in [i]Settings[/i])
 
- When you have finished creating or editing an AI Player, click on the Save button.
+ When you have finished creating or editing an AI Player, click on the Save button. TODO: Make it automatic like HumanScreen.
+"""
 
- Several profiles are included. Have a look at, and experiment with their configurations, or create your own. The AI players starting with a capital letter increase in difficulty alphabetically. Note that the players in [font=%s]*asterisks*[/font] are special, and do not follow any pattern.
+        self.profiles_text = """ Several profiles are included. Have a look at, and experiment with their configurations, or create your own. The AI players starting with a capital letter increase in difficulty alphabetically. Note that the players in [font=%s]*asterisks*[/font] are special, and do not follow any pattern.
 
 """ % (AI_FONT,)
+
+class AIHelpScreen(hs_m.NewHelpScreen):
+    help_text_class = AIHelpText
+
+    #title_text = "AI Player Help"
--- pentai/gui/help_screen.py
+++ pentai/gui/help_screen.py
@@ -4,7 +4,23 @@ from pentai.gui.spacer import *
 from pentai.gui.scrollable_label import *
 from pentai.gui.section import *
 
+from kivy.clock import Clock
+
 class HelpScreen(Screen):
     def on_pre_enter(self):
         self.set_text()
 
+class NewHelpScreen(Screen):
+    title_text = StringProperty("")
+
+    def __init__(self, *args, **kwargs):
+        super(NewHelpScreen, self).__init__(*args, **kwargs)
+        Clock.schedule_once(self.setup_text, 0.5)
+
+    def setup_text(self, *ignored):
+        self.title_text = self.title
+        scrolled = self.ids.scrollable_id
+        scrolled.add_widget(self.help_text_class())
+
+class HelpText(MyScrollable):
+    pass
