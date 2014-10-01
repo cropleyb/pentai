8529e9c5d052bdc4638550f1f3e1cbacecf7502b
cropleyb <cropleyb@yahoo.com.au>
2014-10-01 19:18:17 +1000

    First cut at PyInstaller config for OS X

---
+++ packaging/hook-ZODB.py
@@ -0,0 +1,5 @@
+hiddenimports = ['BTrees.OOBTree', 'BTrees.fsBTree', 
+                 'pkg_resources']
+
+#                 'pkg_resources', 'pywintypes',
+#                  'simo', 'jsonlib2', 'celeryconfig']
